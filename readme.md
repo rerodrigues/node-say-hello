fe1e7bfeb500270a7e26901762430acdb07e7711
Renato Rodrigues <renato.sp@gmail.com>
2024-12-20 03:34:03 +0000

    Initial commit

---
+++ readme.md
@@ -0,0 +1,36 @@
+# node-say-hello
+
+This repo is part of an article series illustrating how to run **Typescript**
+scripts from the command line using **Node.JS**
+
+Check the series [here](https://dev.to/rerodrigues)
+
+## how to run (source)
+
+```
+npm start
+```
+
+alternatively, 
+
+```
+npx tsx src/sayHello.ts
+```
+
+## how to run (transpilled)
+
+```
+npm run build
+```
+
+and then
+
+```
+./dist/sayHello.js
+```
+
+alternatively, 
+
+```
+node dist/sayHello.js
+```
---
+++ src/sayHello.ts
@@ -0,0 +1,46 @@
+#!/usr/bin/env -S npx tsx
+
+import inquirer, { Answers } from "inquirer";
+import chalk from "chalk";
+import yosay from "yosay";
+
+console.log(chalk.blue("\nHello visitor, please enter you details:\n"));
+
+const questions: Answers = [
+  {
+    type: "input",
+    name: "name",
+    message: "What's your name?",
+    required: true,
+  },
+  {
+    type: "list",
+    name: "salutation",
+    message: "How should I refer to you?",
+    choices: ["Mr.", "Mrs.", "none"],
+  },
+  {
+    type: "confirm",
+    name: "isEverythingOk",
+    message: "Is the entered information correct?",
+    default: true,
+    transformer: (answer) => (answer ? "👍" : "👎"),
+  },
+];
+
+inquirer.prompt(questions).then((answers) => {
+  const { isEverythingOk, salutation, name } = answers;
+
+  if (!isEverythingOk) {
+    console.log(
+      `\n${chalk.red("[Error]:")} please try again with the correct information`
+    );
+    return false;
+  }
+  console.log(`\n${chalk.green("[SUCCESS]:")} information entered was correct`);
+
+  const person: string[] = [name];
+  if (salutation !== "none") person.unshift(salutation);
+
+  console.log(yosay(`Hallo ${person.join(" ")}!`));
+});
